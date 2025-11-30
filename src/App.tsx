31aa8f8b7bd64d368837440ff53e5d6b7fecfd7e
heyylateef <lateef.lotz@gmail.com>
2025-11-29 19:01:02 -0500

    added name

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Lateef</h1>
+      <h1>Vite + React + Adetona</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
